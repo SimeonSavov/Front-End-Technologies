c4f560532aa43d8312794e7d05b229756ad65643
Simeon <simeonsavov21@gmail.com>
2024-05-24 10:32:00 +0300

    feat: Add folders with JavaScript Async functions exercises

---
+++ 06. JS-Async-Exercises - Part 2/07-Throttling-Promises/app.js
@@ -0,0 +1,33 @@
+async function throttlePromises() {
+    const asyncTasks = [
+        () => new Promise(resolve => setTimeout(() => { console.log("Task 1"); resolve("Task 1 done") }, 1000)),
+        () => new Promise(resolve => setTimeout(() => { console.log("Task 2"); resolve("Task 2 done") }, 1500)),
+        () => new Promise(resolve => setTimeout(() => { console.log("Task 3"); resolve("Task 3 done") }, 500)),
+        () => new Promise(resolve => setTimeout(() => { console.log("Task 4"); resolve("Task 4 done") }, 2000))
+    ];
+
+    async function throttle(tasks, limit) {
+        const results = [];
+        const executing = [];
+
+        for (const task of tasks) {
+            const p = task().then(result => {
+                executing.splice(executing.indexOf(p), 1);
+                return result;
+            });
+
+            executing.push(p);
+            results.push(p);
+            
+            if (executing.length >= limit) {
+                await Promise.race(executing);
+            }
+        }
+
+
+        return Promise.all(results);
+    }
+
+    const results = await throttle(asyncTasks, 2);
+    console.log("All task are done", results);
+}
